f46f7c688f4079203e4c1d8448ef34c714c08f1d
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2023-03-08 16:53:46 -0300

    .

--- exercicios/ex17/repetição.js
+++ exercicios/ex17/repetição.js
@@ -1,14 +1,10 @@
-var c = 1;
+// var c = 1;
 // while(c < 6){
 //     console.log(c)
 //     c++
 // }
-// do {
-//     console.log(c);
-//     c++;
-// } while (c <= 10);
-// console.log("ìnicio");
-
+/////////////////////////////////////////////////////
+console.log("ìnicio");
 for (var c = 1; c <= 100; c++) {
     console.log(c);
 }
