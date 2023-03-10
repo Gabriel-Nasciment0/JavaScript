18a16480615400b2d093a6c0ed878294cf3c3ef3
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2023-03-10 14:00:10 -0300

    .

---
+++ exercicios/ex17,2/script.js
@@ -0,0 +1,10 @@
+function contar() {
+    var inicio = Number(document.getElementById("inicio").value);
+    var fim = Number(document.getElementById("fim").value);
+    var passo = Number(document.getElementById("passo").value);
+    var resultado = document.getElementById("resultado");
+
+    while(inicio <= fim)
+    resultado.innerHTML = inicio
+
+}
