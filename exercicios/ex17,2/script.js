459ecbf1864a1e0e87deb849e15b990bbc1beb95
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2023-03-10 16:01:17 -0300

    .

--- exercicios/ex17,2/script.js
+++ exercicios/ex17,2/script.js
@@ -1,10 +1,22 @@
 function contar() {
-    var inicio = Number(document.getElementById("inicio").value);
-    var fim = Number(document.getElementById("fim").value);
-    var passo = Number(document.getElementById("passo").value);
+    let inicio = document.getElementById("inicio");
+    let fim = document.getElementById("fim");
+    let passo = document.getElementById("passo");
     var resultado = document.getElementById("resultado");
-
-    while(inicio <= fim)
-    resultado.innerHTML = inicio
-
+    if (
+        inicio.value.length == 0 ||
+        fim.value.length == 0 ||
+        passo.value.length == 0
+    ) {
+        resultado.innerHTML = "erro";
+    } else {
+        resultado.innerHTML = `Contando:<br>`;
+        let ini = Number(inicio.value);
+        let FIM = Number(fim.value);
+        let pass = Number(passo.value);
+        for (let c = ini; c <= FIM; c += pass) {
+            resultado.innerHTML += `${c} 👉`;
+        }
+        resultado.innerHTML += `🏁`;
+    }
 }
