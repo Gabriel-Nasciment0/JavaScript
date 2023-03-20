740455e1c12e5651b3a70c99a3a8560bc6eb7d47
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2023-03-20 15:04:30 -0300

    .

--- exercicios/ex18/ambiente.js
+++ exercicios/ex18/ambiente.js
@@ -1,12 +1,22 @@
-// let num = [5, 8, 2, 9, 3];
-// num.push(7);
-// num.push(4);
-// num.push(1);
-// num.push(6);
-// num.sort();
-// console.log(num);
-// console.log(`O vetor tem ${num.length} posições`);
-// console.log(`O primeiro valor do vetor e ${num[0]}`);
-for(let pos=0 ; pos < Num.length;pos++){
-    console.log(num[pos])
+// variaveis compostas
+let num = [5, 8, 2, 9, 3];
+// acrescenta valores a um array
+num.push(7);
+num.push(4);
+num.push(1);
+num.push(6);
+// ordena os numeros
+num.sort();
+// num.length informa quantos elementos tem em um array
+console.log(num);
+console.log(`O vetor tem ${num.length} posições`);
+console.log(`O primeiro valor do vetor e ${num[0]}`);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+let pos = num.indexOf(8);
+if (pos == -1) {
+    console.log("O valor não foi encontrado");
+} else {
+    console.log(`O valor 8 esta na posição ${pos}`);
 }
