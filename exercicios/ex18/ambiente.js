555486de3a2f6c6455981705b1e78e618fcecadb
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2023-03-20 14:13:39 -0300

    .

---
+++ exercicios/ex18/ambiente.js
@@ -0,0 +1,12 @@
+// let num = [5, 8, 2, 9, 3];
+// num.push(7);
+// num.push(4);
+// num.push(1);
+// num.push(6);
+// num.sort();
+// console.log(num);
+// console.log(`O vetor tem ${num.length} posições`);
+// console.log(`O primeiro valor do vetor e ${num[0]}`);
+for(let pos=0 ; pos < Num.length;pos++){
+    console.log(num[pos])
+}
