c6faaeb7377d177a41387addc81a341867fb202c
Gabriel-Nasciment0 <116118477+Gabriel-Nasciment0@users.noreply.github.com>
2023-03-23 12:51:33 -0300

    .

---
+++ exercicios/ex19/ex/script.js
@@ -0,0 +1,8 @@
+let num = document.querySelector("input#numero");
+let lista = document.querySelector("select#lista");
+let res = document.querySelector("div#res");
+let vetor = []
+
+function adicionar() {
+    
+}
